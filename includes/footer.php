f77c4bf370a972057fba2d1ffebe7afb50f9368c
mesenkendal <mesenkendal@gmail.com>
2025-12-18 22:33:05 -0600

    footer

--- includes/footer.php
+++ includes/footer.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 ?>
 <footer class="app-footer">
     <div class="footer-inner">
-        <span>Proyecto graduacion Kendall Mesen 2025</span>
+        <span>Proyecto graduacion Kendall Mesen y Josue Arrieta Alfaro 2025</span>
     </div>
 </footer>
