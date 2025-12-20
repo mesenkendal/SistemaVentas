3fc1b18fe6c4b65f54e622ab4c2ea10a39b0d0fa
mesenkendal <mesenkendal@gmail.com>
2025-12-19 22:02:48 -0600

    acerca

--- public/acerca.php
+++ public/acerca.php
@@ -168,7 +168,7 @@ $pdfPath = 'uploads/manual.pdf.pdf';
     <?php 
 // --- CONFIGURACIÓN DEL NOMBRE DEL ARCHIVO ---
 // 1. Asegúrate de que el nombre aquí sea IGUAL al que subiste a GitHub
-$nombreRealDelArchivo = 'Manual_de_usuario.docx'; 
+$nombreRealDelArchivo = 'manualU.pdf'; 
 $pdfPath = 'uploads/' . $nombreRealDelArchivo; 
 ?>
 
